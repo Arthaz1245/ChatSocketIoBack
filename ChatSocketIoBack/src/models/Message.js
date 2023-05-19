edb380f0355c22a96b71de925ecda17e2f0d54ad
Arthaz1245 <sergiomendozap96@gmail.com>
2023-05-19 14:18:30 -0500

    new chages

--- ChatSocketIoBack/src/models/Message.js
+++ ChatSocketIoBack/src/models/Message.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const messageSchema = new mongoose.Schema(
   { chatId: String, senderId: String, text: String },
-  { versionKey: false }
+  { timestamps: true }
 );
 
 const Message = mongoose.model("Message", messageSchema);
